8ccc38ba797f512a53d39cc738f8f8faa9f1afeb
Muhammad Choudhary <choudha1@sheridancollege.ca>
2022-08-04 12:45:42 -0400

    Install all basic packages

---
+++ index.js
@@ -0,0 +1,8 @@
+// initialize Express in project
+const express = require('express');
+const app = express();
+
+// start Express on port 8080
+app.listen(8080, () => {
+    console.log('Server Started on http://localhost:8080');
+});
