c9f3e455289f1c81bb61356f3ca5401a8b7838a5
Muhammad Choudhary <choudha1@sheridancollege.ca>
2022-08-08 16:59:22 -0400

    Complete joining table and returning posts table

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,11 +1,32 @@
 const knex = require('knex')(require('../knexfile').development);
 
 exports.index = (_req, res) => {
-  knex('posts')
-    .then((data) => {
-      res.status(200).json(data);
-    })
-    .catch((err) =>
-      res.status(400).send(`Error retrieving Inventories: ${err}`)
-    );
+//   knex('posts')
+//     .then((data) => {
+//       res.status(200).json(data);
+//     })
+//     .catch((err) =>
+//       res.status(400).send(`Error retrieving Inventories: ${err}`)
+//     );
+
+
+    knex
+        .select(
+            "posts.id",
+            "posts.user_id",
+            "posts.title",
+            "posts.content",
+            "posts.updated_at",
+            "users.id",
+            "users.username as username"
+        )
+        .from("posts")
+        .leftJoin("users", "posts.user_id", "users.id")
+        .orderBy("posts.id", "desc")
+        .then((data) => {
+                  res.status(200).json(data);
+                })
+                .catch((err) =>
+                  res.status(400).send(`Error retrieving posts: ${err}`)
+                );
 };
